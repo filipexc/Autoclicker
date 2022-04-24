9921828da010011943271fd569f11d102ee694aa
Filipe <filipepereira@id.uff.br>
2022-04-24 01:04:28 -0300

    AutoClickers in python
    version 1

---
+++ alternative.py
@@ -0,0 +1,6 @@
+from time import sleep
+import pyautogui as pg
+
+while True:
+   pg.click()
+   sleep(0.5) #or set it lower if you want it even more faster. 
---
+++ main.py
@@ -0,0 +1,27 @@
+import time
+import threading
+from pynput.keyboard import Listener, KeyCode
+from pynput.mouse import Controller , Button
+
+Toggle_key = KeyCode(char='t')
+
+clicking = False
+mouse = Controller()
+
+def clicker():
+    while True:
+        if clicking:
+            mouse.click(Button.left , 1)
+        time.sleep(0.0001)
+      
+def toggle_event(key):
+    if key == Toggle_key:
+        global clicking
+        clicking = not clicking
+
+
+click_thread = threading.Thread(target= clicker)t
+click_thread.start()
+
+with Listener(on_press= toggle_event) as listener:
+    listener.join()
